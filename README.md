f44e074951cad8dfebb417e0accb499e0ccaba7b
Tsigie Kumssa <tktsigie@yahoo.com>
2018-06-19 14:20:44 -0400

    Added JS

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Shopping_List
+
+Small Shopping list just for practicing.
--- assets/script.js
+++ assets/script.js
@@ -1,29 +1,77 @@
-var button = document.getElemntById("enter");
+var button = document.getElementById("enter");
 var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
-
-function inputLength() {
-	return input.value.length;
+var listItems = document.getElementByTagName("Li");
+var deleteBtn = document.getElementByTagName("button");
+//get length of list
+function listLength() {
+	console.log(listItems.length);
+	return listItems.length;
 }
 
-function createListElement() {
+//create delete buttons for existing buttons
+
+for (i = 0; i < listLength(); i++ ) {
+	var btn = document.createElement("button");
+			btn.appendChild(document.createTextNode("Delete!"));
+			listItems[i].appendChild(btn);
+			btn.onclick = removeParent;
+}
+
+//Remove an item from the list
+
+function removeParent(evt) {
+	evt.target.removeEventListner("click", removeParent, false);
+	evt.target.parentNode.remove();
+}
+
+//Check or uncheck list items
+
+function checked(event) {
+	if (event.target.tagName === "Li") {
+			event.target.classList.toggle("done");
+	}
+}
+
+//Get the button array
+
+function deleteBtnsLength() {
+	console.log(deleteBtn.length);
+	return deleteBtn.length;
+};
+
+// Create a new list item for user input
+
+function createElement() {
 	var li = document.createElement("li");
-	li.appendChild(document.createTextNode(input.value));
-	ui.appendChild(li);
-	input.value = "";
-}
+		li.appendChild(document.createTextNode(input.value));
+			ui.appendChild(li);
+			input.value = "";
 
-function addListAfterClick() {
+	// Create delete button for each new list item
+	var btn = document.createElement("button");
+			btn.appendChild(document.createTextNode("Delete"));
+			li.appendChild(btn);
+			btn.onclick = removeParent;
+};
+
+// User click add input
+
+function addListByClick() {
 	if (inputLength() > 0) {
-		createListElement();
+		createElement();
 	}
-}
+};
 
-function addListAfterKeypress(event) {
+// User press enter on keyboard
+
+function addListByKeypress(event) {
 	if (inputLength() > 0 && event.keyCode === 13) {
-		createListElement();
+		//event.which works too!
+		createElement();
 	}
-}
+};
 
-button.addEventListner("click", addListAfterClick);
-input.addEventListner("keypress", addListAfterKeypress);
+ul.addEventListner("click", checked);
+button.addEventListner("click", addElementByClick);
+input.addEventListner("keypress", addElementByKeypress);
