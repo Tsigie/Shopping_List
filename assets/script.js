03a022bfe06ff3a94ca183a88b790aa82dcedd9e
Tsigie Kumssa <tktsigie@yahoo.com>
2018-06-19 12:07:32 -0400

    Added files

---
+++ assets/script.js
@@ -0,0 +1,29 @@
+var button = document.getElemntById("enter");
+var input = document.getElementById("userinput");
+var ul = document.querySelector("ul");
+
+function inputLength() {
+	return input.value.length;
+}
+
+function createListElement() {
+	var li = document.createElement("li");
+	li.appendChild(document.createTextNode(input.value));
+	ui.appendChild(li);
+	input.value = "";
+}
+
+function addListAfterClick() {
+	if (inputLength() > 0) {
+		createListElement();
+	}
+}
+
+function addListAfterKeypress(event) {
+	if (inputLength() > 0 && event.keyCode === 13) {
+		createListElement();
+	}
+}
+
+button.addEventListner("click", addListAfterClick);
+input.addEventListner("keypress", addListAfterKeypress);
